fc9aa0aa44a2a289ad475b660be51da703329d23
John-Philip Johansson <seriema@gmail.com>
2022-12-11 11:00:05 +0100

    Use foreach for even cleaner loop

--- generate-links.ps1
+++ generate-links.ps1
@@ -1,7 +1,6 @@
 $files = Get-ChildItem .
 
-for ($i=0; $i -lt $files.Count; $i++) {
-	$file = $files[$i]
+foreach ($file in $files) {
 	if ($file.Extension -like ".md" -and $file.BaseName -notlike "README") {
 		"* [{0}]({1})" -f $file.BaseName, $file.Name
 	}
