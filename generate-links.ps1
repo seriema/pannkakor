3311a6af13b7529f47d6a03573bf37509af57c48
John-Philip Johansson <seriema@gmail.com>
2022-12-11 10:57:22 +0100

    Filter out non-recipe files from links

--- generate-links.ps1
+++ generate-links.ps1
@@ -1,5 +1,7 @@
 $files = Get-ChildItem .
 
 for ($i=0; $i -lt $files.Count; $i++) {
-	"* [{0}]({1})" -f $files[$i].BaseName, $files[$i].Name
+	if ($files[$i].Extension -like ".md" -and $files[$i].BaseName -notlike "README") {
+		"* [{0}]({1})" -f $files[$i].BaseName, $files[$i].Name
+	}
 }
